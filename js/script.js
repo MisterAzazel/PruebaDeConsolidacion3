6a839b75e1ec5800b2cb6edb77ac93d84f2967d6
Josué Peñaloza <josue20650@gmail.com>
2024-08-03 21:31:40 -0400

    Trabajo terminado, no es lo mejor, pero algo es algo

--- js/script.js
+++ js/script.js
@@ -7,19 +7,16 @@ const media = window.matchMedia('(max-width: 768px)')
     function setupListeners() {
         if (media.matches) {
             chatSection.style.visibility = 'hidden';
-            // Solo agregar los listeners si estamos en pantallas pequeñas
             contactItems.forEach(item => {
                 item.addEventListener('click', handleContactClick);
             });
             back.addEventListener('click', handleBackClick);
         } else {
-            // Eliminar los listeners si estamos en pantallas grandes
             contactItems.forEach(item => {
                 item.removeEventListener('click', handleContactClick);
             });
             back.removeEventListener('click', handleBackClick);
 
-            // Asegurarse de que los elementos se muestren correctamente en pantallas grandes
             chatSection.style.visibility = 'visible';
             contactsSection.style.display = 'block';
         }
@@ -40,43 +37,4 @@ const media = window.matchMedia('(max-width: 768px)')
 
     // Actualizar los listeners cuando cambie el tamaño de la pantalla
     media.addListener(setupListeners);
-
-
-/*document.addEventListener("DOMContentLoaded", function() {
-    const contactItems = document.querySelectorAll('.contacts-li');
-    const chatSection = document.getElementById('chat');
-    const contactsSection = document.getElementById('contacts')
-    const back = document.getElementById('back')
-    const media = window.matchMedia('(max-width: 768px)')
-
-    function screenTest(e){
-        if(e.matches){
-            contactItems.forEach(item => {
-                item.addEventListener('click', function() {
-                    chatSection.style.visibility = 'visible';
-                    contactsSection.style.display = 'none';
-                });
-            });
-
-
-            back.addEventListener('click', function(){
-                chatSection.style.visibility = 'hidden';
-                contactsSection.style.display = 'block';
-            }
-
-            )
-
-        }
-        else{
-            chatSection.style.visibility = 'visible'
-            contactsSection.style.display = 'block';
-        }
-
-    }
-
-    media.addListener(screenTest);
-
-});
-*/
-
 
